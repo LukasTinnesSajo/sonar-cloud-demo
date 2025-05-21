e662385279c18cbb9f520cc5567b30b1a4abf176
Lukas Tinnes <lukas.tinnes@sajoconsulting.de>
2025-05-21 13:57:11 +0200

    pipe works now

--- src/main/java/com/example/demo/QuickFixDemo.java
+++ src/main/java/com/example/demo/QuickFixDemo.java
@@ -1,23 +1,8 @@
 package com.example.demo;
 
 public class QuickFixDemo {
-    private int unusedField = 42; // Unused field (code smell)
 
     public int add(int a, int b) {
-        // Simple logic, duplicated below
         return a + b;
-    }
-
-    public int sum(int a, int b) {
-        // Duplicate logic to trigger SonarQube duplication rule
-        return a + b;
-    }
-
-    public void handleException() {
-        try {
-            int x = 1 / 0;
-        } catch (Exception e) {
-            // Empty catch block (code smell)
-        }
     }
 }
--- src/test/java/com/example/demo/QuickFixDemoTest.java
+++ src/test/java/com/example/demo/QuickFixDemoTest.java
@@ -9,17 +9,4 @@ public class QuickFixDemoTest {
         QuickFixDemo demo = new QuickFixDemo();
         assertEquals(5, demo.add(2, 3));
     }
-
-    @Test
-    void testSum() {
-        QuickFixDemo demo = new QuickFixDemo();
-        assertEquals(7, demo.sum(3, 4));
-    }
-
-    @Test
-    void testHandleException() {
-        QuickFixDemo demo = new QuickFixDemo();
-        // Should not throw
-        demo.handleException();
-    }
 }
